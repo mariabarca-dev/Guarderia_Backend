7efe30b19272d04a4665e51de5fd03f963df4830
hector <hector.machaca@alu.inspt.utn.edu.ar>
2026-09-23 19:57:07 -0300

    me equivoque hal correr el programa me tiva un error

--- src/main/java/com/guarderiaCentral/guarderia_Backend/dtos/AdministradorDTO.java
+++ src/main/java/com/guarderiaCentral/guarderia_Backend/dtos/AdministradorDTO.java
@@ -1,24 +1,28 @@
 package com.guarderiaCentral.guarderia_Backend.dtos;
 
-import lombok.AllArgsConstructor;
+import com.guarderiaCentral.guarderia_Backend.modelos.Rol;
 import lombok.Data;
 import lombok.EqualsAndHashCode;
 import lombok.NoArgsConstructor;
 import lombok.ToString;
 
 /**
- * DTO que representa a un Administrador del sistema para las transferencias de datos en la API REST.
- * Extiende de {@link UsuarioDTO} y no contiene lógica de negocio ni anotaciones JPA.
+ * DTO que representa a un Administrador del sistema.
+ * Extiende de {@link UsuarioDTO} y no contiene atributos propios ni lógica JPA.
  *
  * @author Franco Buyatti, Daniela Forclaz, Héctor Machaca, María Eugenia Barca
- * @version 1.0
  */
 @Data
 @NoArgsConstructor
-@AllArgsConstructor
 @EqualsAndHashCode(callSuper = true)
 @ToString(callSuper = true)
 public class AdministradorDTO extends UsuarioDTO {
 
-
+    /**
+     * Constructor completo que delega todos los atributos al constructor de la clase padre.
+     */
+    public AdministradorDTO(int id, String nombre, String apellido, String direccion,
+                            String telefono, String nombreUsuario, String clave, Rol rol, Boolean activo) {
+        super(id, nombre, apellido, direccion, telefono, nombreUsuario, clave, rol, activo);
+    }
 }
